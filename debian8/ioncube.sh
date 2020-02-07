5e8060c96cd5d1204a9e86fb4bbbe9e664959a8b
BSPANEL <vitya123vitya.ua@gmail.com>
2020-02-07 13:45:16 +0300

    Update ioncube.sh

--- debian8/ioncube.sh
+++ debian8/ioncube.sh
@@ -24,27 +24,10 @@ check()
   fi
 }
 echo "• Приступаем к установке ${red}Ioncube Loader${green} •"
-MODULES=$(php -i | grep extension_dir | awk '{print $NF}')
-PHP_VERSION=$(php -r "echo PHP_MAJOR_VERSION.'.'.PHP_MINOR_VERSION;")
-ARCH=$(getconf LONG_BIT)
-
-# Если 32-бит
-if [ $ARCH == "32" ]; then
-  echo "${Cyan} Скачивание Ioncube Loader.. ${Color_Off}"
-  wget http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86.tar.gz > /dev/null 2>&1
-
-  echo "${Cyan} Распаковка Ioncube Loader.. ${Color_Off}"
   tar xvfz ioncube_loaders_lin_x86.tar.gz > /dev/null 2>&1
-# Если 64-бит
-else
-  echo "${Cyan} • Скачивание Ioncube Loader • ${Color_Off}"
-  wget http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz > /dev/null 2>&1 && check
-  echo "${Cyan} • Распакуем файл • ${Color_Off}"
-  tar xvfz ioncube_loaders_lin_x86-64.tar.gz > /dev/null 2>&1 && check
-fi
-rm /root/ioncube_loaders_lin_x86-64.tar.gz /root/ioncube_loaders_lin_x86.tar.gz > /dev/null 2>&1
-echo "${Cyan} • Копирования файлов Ioncube Loader • ${Color_Off}" && check
-sudo cp /root/ioncube/ioncube_loader_lin_${PHP_VERSION}.so $MODULES > /dev/null 2>&1
-sed -i "3i\zend_extension = ${MODULES}/ioncube_loader_lin_${PHP_VERSION}.so" /etc/php5/apache2/php.ini > /dev/null 2>&1
-rm -r /root/ioncube > /dev/null 2>&1
-echo "${Cyan} Копирования успешно прошла!${Color_Off}" && check
+wget http://core.brainycp.com/src/package/ioncube_el6_x86-64.tar.gz &>/dev/null
+tar zxf ioncube_el6_x86-64.tar.gz
+cp /etc/brainy/src/ioncube/ioncube_loader_lin_5.6.so /etc/brainy/src/compiled/php5/ioncube_loader_lin_5.6.so
+cp /etc/brainy/src/ioncube/ioncube_loader_lin_5.6_ts.so /etc/brainy/src/compiled/php5/ioncube_loader_lin_5.6_ts.so
+echo "zend_extension=ioncube_loader_lin_5.5.so">>"/etc/php5/apache2/php.ini"
+echo "zend_extension=ioncube_loader_lin_5.5.so">>"/etc/php5/cli/php.ini"
